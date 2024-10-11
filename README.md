06718e72267c50ae946e631afcf04dc7de660005
Abeeb Raheem <belovetech@gmail.com>
2024-10-11 08:49:40 +0100

    (doc): update readme documentation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # fileRecon
-File reconciliation
+
+This project compares two CSV files to identify missing records and discrepancies between them. It generates a comprehensive report that highlights differences, allowing users to easily spot inconsistencies and ensure data integrity.
+
+## Setup Instructions
+
+1. Clone the repository.
+2. Create a virtual environment: `python -m venv .venv`
+3. Install dependencies: `pip install -r requirements.txt`
+4. Run the migrations: `python manage.py migrate`
+5. Start the development server: `python manage.py runserver`
+
+## Usage
+
+BASE URL`http://localhost:8000/api/`
+
+1. Upload files at `upload/`.
+2. Reconcile files at `api/reconcile/<file_id>?format=json|csv|html`.
+
+# Run tests
+
+` python manage.py test reconciliation`
--- reconciliation/tests.py
+++ reconciliation/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# Create your tests here.
+
+class FileUploadViewTests(APITestCase):
+    def setUp(self):
+        self.upload_url = reverse('file-upload')
+        self.test_source_file = SimpleUploadedFile(
+            'source.csv',
+            b'ID,Name,Date,Amount\n001,John Doe,2023-01-01,100.00\n002,Jane Smith,2023-01-02,200.00'
+        )
+        self.test_target_file = SimpleUploadedFile(
+            'target.csv',
+            b'ID,Name,Date,Amount\n001,John Doe,2023-01-01,150.00\n002,Jane Smith,2023-01-02,200.00'
+        )
+
+    def test_upload_files_success(self):
+        response = self.client.post(self.upload_url, {
+            'source_file': self.test_source_file,
+            'target_file': self.test_target_file,
+        }, format='multipart')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('message', response.data)
+        self.assertIn('id', response.data)
+
+    def test_upload_files_invalid(self):
+        response = self.client.post(self.upload_url, {
+        }, format='multipart')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('source_file', response.data)
+        self.assertIn('target_file', response.data)
+
+    def test_get_reconciliation_success(self):
+        upload_response = self.client.post(self.upload_url, {
+            'source_file': self.test_source_file,
+            'target_file': self.test_target_file,
+        }, format='multipart')
+
+        file_id = upload_response.data['id']
+        reconciliation_url = reverse(
+            'reconcile-files', args=[file_id])
+
+        response = self.client.get(reconciliation_url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('missing_in_target', response.data)
+        self.assertIn('missing_in_source', response.data)
+        self.assertIn('discrepancies', response.data)
+
+    def test_get_reconciliation_file_not_found(self):
+        response = self.client.get(
+            reverse('reconcile-files', args=[999]), format='json')  # Non-existent ID
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertIn('error', response.data)
+        self.assertEqual(response.data['error'], "File not found")
+
+    def tearDown(self):
+        pass

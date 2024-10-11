f92dd5b02661f0911c784a677937227863b4f0c4
Abeeb Raheem <belovetech@gmail.com>
2024-10-11 02:09:29 +0100

    (chore): implement reconcile file views

--- reconciliation/views.py
+++ reconciliation/views.py
@@ -3,6 +3,8 @@ from django.core.exceptions import ValidationError
 from rest_framework import status, views
 from rest_framework.response import Response
 from reconciliation.serializers import ReconciliationFileSerializer
+from reconciliation.models import ReconciliationFile
+from reconciliation.utils import reconcile_files
 
 
 logger = logging.getLogger(__name__)
@@ -40,3 +42,22 @@ class FileUploadView(views.APIView):
         if file is None:
             return False
         return file.name.endswith('.csv') or file.content_type == 'text/csv'
+
+    def get(self, request, file_id, format=None):
+        try:
+            reconciliation_file = ReconciliationFile.objects.get(id=file_id)
+            source_file = reconciliation_file.source_file.path
+            target_file = reconciliation_file.target_file.path
+
+            response_data = reconcile_files(source_file, target_file)
+            return Response(response_data, status=status.HTTP_200_OK)
+
+        except ReconciliationFile.DoesNotExist:
+            logger.error(f"Reconciliation file with ID {file_id} not found.")
+            return Response({"error": "File not found"}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            logger.error(f"Error during reconciliation: {e}")
+            if e.args[0] == 'source and target headers do not match':
+                return Response({"error": "Source and target columns do not match"}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response({"error": "Unable to reconcile files"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
